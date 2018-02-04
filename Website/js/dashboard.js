a9e1217c4e526cdd71da68468b3750a703e8f711
Austin Shin <amshin1775@gmail.com>
2018-02-04 01:05:21 -0800

    Start dashboard

--- Website/js/dashboard.js
+++ Website/js/dashboard.js
@@ -1,10 +1,34 @@
 (function(){
-	firebase.auth().onAuthStateChanged(function(currentUser) {
+	var config = {
+		apiKey: "AIzaSyD1Vdfz3VAiFbGMn8ZrSTpc44YLJhx75fQ",
+		authDomain: "testproj-73158.firebaseapp.com",
+		databaseURL: "https://testproj-73158.firebaseio.com",
+		projectId: "testproj-73158",
+		storageBucket: "testproj-73158.appspot.com",
+		messagingSenderId: "604434254562"
+	};
+	firebase.initializeApp(config);
+
+	/*firebase.auth().onAuthStateChanged(function(currentUser) {
 	  if (currentUser) {
 	    // the user is logged in, you can bootstrap functionality now
+	    init();
 	  }
 	  else{
 	  	window.location = '../html/404.html';
 	  }
-	});
+	});*/
+	init();
 }());
+
+function init(){
+
+	//Make a Trip for every trip in the input from database
+	/*var trip = document.createElement("LI");
+	 *add information to trip (make nodes)
+	 textNode = document.createTextNode("text");
+	 *trip.append(textNode);
+	 *document.getElementById("tripEntries").appendChild(trip);
+	*/
+
+}
