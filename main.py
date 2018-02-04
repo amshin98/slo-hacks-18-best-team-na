865399a1299a386c952f3971e82e4020cc0f8854
Zach Blalock <zl.blalock@gmail.com>
2018-02-03 20:28:57 -0800

    added maps api access
    make sure to re-install requiremnts.txt before running

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import Flask, redirect, render_template, request
+import googlemaps
+from googlemaps import distance_matrix
+import json
 
 app = Flask(__name__)
-
+maps_client = googlemaps.Client(key='AIzaSyBQyKQQh6QIdO_yP_2iBcveuJM5a5GIUkg')
 
 @app.route('/')
 def homepage():
     # Return a Jinja2 HTML template and pass in image_entities as a parameter.
     return render_template('homepage.html')
 
+
+@app.route('/anotherpage', methods=['GET', 'POST'])
+def anotherpage():
+    to_input = request.form['to_addr']
+    from_input = request.form['from_addr']
+    bike_json = distance_matrix.distance_matrix(maps_client, from_input, to_input, mode='bicycling')
+    bike_time = bike_json['rows'][0]['elements'][0]['duration']['text']
+    walk_json = distance_matrix.distance_matrix(maps_client, from_input, to_input, mode='walking')
+    walk_time = walk_json['rows'][0]['elements'][0]['duration']['text']
+    return render_template('anotherpage.html', bike_time=bike_time, walk_time=walk_time)
 
 @app.errorhandler(500)
 def server_error(e):
