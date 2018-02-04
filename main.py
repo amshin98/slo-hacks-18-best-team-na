0230120043e324cb0b12be4f8ed95f65430f9dae
Zach Blalock <zl.blalock@gmail.com>
2018-02-04 05:01:51 -0800

    added details to dashboard, client available as a js variable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request
+import decimal
 from uber_rides.auth import AuthorizationCodeGrant
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
@@ -39,17 +40,32 @@ def dashboard():
     session = auth_flow.get_session(session_url)
     client = UberRidesClient(session, sandbox_mode=True)
     credentials = session.oauth2credential
-    print("session is type " + str(type(credentials)))
     # TODO store the credentials in the database
-    response = client.get_user_profile()
-    profile = response.json
-    fname = profile.get('first_name')
-    lname = profile.get('last_name')
-    email = profile.get('email')
     response = client.get_user_activity()
     history = response.json
+    for x in range(len(history['history'])):
+        history['history'][x]['distance'] = round(history['history'][x]['distance'], 2)
     print(history)
-    return render_template('dashboard.html', history=history)
+    price_list = []
+    for ride in history['history']:
+        price_list.append(get_estimated_price(ride['start_time'], ride['end_time'], ride['distance']))
+    print(price_list)
+    walk_matrix = [['1011 Railroad Ave, San Luis Obispo, CA 93401, US', 'Cerro Hollister, San Luis Obispo, CA 93405, USA'],
+                   ['1701 Grand Ave, Del Mar, CA 92014, USA', '3663 Lorimer Ln, Encinitas, CA 92024, USA'],
+                   ['3951 Camino Calma, San Diego, CA 92122, USA', '507 Kristen Ct, Encinitas, CA 92024, US'],
+                   ['100 Grand Ave, San Luis Obispo, CA 93405, USA', '740 W Foothill Blvd, San Luis Obispo, California 93405, US'],
+                   ['1011 Railroad Ave, San Luis Obispo, CA 93401, US', 'Cerro Hollister, San Luis Obispo, CA 93405, USA']
+                   ]
+    walking_times = []
+    for to_from in walk_matrix:
+        dist_mx = distance_matrix.distance_matrix(maps_client, to_from[0], to_from[1], mode='walking')
+        walking_times.append(dist_mx['rows'][0]['elements'][0]['duration']['text'])
+    user_data = {
+        'history': history,
+        'price_list': price_list,
+        'walking_times': walking_times
+    }
+    return render_template('dashboard.html', user_data=user_data, credentials=credentials)
 
 
 @app.route('/anotherpage', methods=['GET', 'POST'])
@@ -68,6 +84,11 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
+def get_estimated_price(start_time, end_time, distance):
+    tot_time = end_time - start_time
+    two_places = decimal.Decimal(10) ** -2
+    return decimal.Decimal((tot_time/60) * 0.85 + distance*2).quantize(two_places)
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
