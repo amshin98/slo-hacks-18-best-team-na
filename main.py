394b2ae402d4fbbae1ed933e38223fe62094d0c1
Zach Blalock <zl.blalock@gmail.com>
2018-02-03 17:40:30 -0800

    added some basic shit to the main.py class and added a folder for html code

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    # Return a Jinja2 HTML template and pass in image_entities as a parameter.
+    return render_template('homepage.html')
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
 
